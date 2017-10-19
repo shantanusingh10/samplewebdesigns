bc35f7371457c7e0c8b97fadd4f596a1935b9024
Shantanu <shantanusingh994@gmail.com>
2017-10-19 15:56:14 +0530

    Replaced with optimized version
    Added optimization related to table cells and active total rows field

--- test2/main.js
+++ test2/main.js
@@ -1,5 +1,6 @@
 var startingIndex = 1;
 var noOfRows = 10;
+var totalRows = 100;
 
 var secondCell = [];
 var thirdCell = [];
@@ -11,7 +12,7 @@ var OrdId = [];
 var KsnId = [];
 var SkuCd = [];
 
-for(var i=0;i<100;i++){
+for(var i=0;i<1000000;i++){
 	Locn.push(Math.floor(10000*Math.random()));
 	OrdId.push(Math.floor(10000*Math.random()));
 	KsnId.push(Math.floor(10000*Math.random()));
@@ -31,10 +32,18 @@ $("#startRows").change(function(){
 	$(".data").remove();
 	populate();
 });
+$("#totalRows").change(function(){
+	totalRows = parseInt($("#totalRows").val());
+	if(totalRows<parseInt($("#startRows").val())){
+		alert("Total rows can't be less than starting row");
+	}
+	$(".data").remove();
+	populate();
+});
 
 function populate(){
 	for(var i=startingIndex-1;i<noOfRows+startingIndex-1;i++){
-		if(i>99)break;	
+		if(i>totalRows-1)break;	
 		$("table").append("<tr class='data'><td class='firstcell'></td><td class='secondcell'></td><td class='thirdcell'></td><td class='fourthcell'></td><td class='fifthcell'></td></tr>");	
 		
 	}
@@ -45,7 +54,7 @@ function populate(){
 	fifthCell = document.querySelectorAll(".fifthcell");
 
 	for(var i=startingIndex-1,j=1;i<noOfRows+startingIndex-1;i++,j++){
-		if(i>99)break;
+		if(i>totalRows-1)break;
 		if(secondCell[j] == undefined || thirdCell[j] == undefined || fourthCell[j] == undefined || fifthCell[j] == undefined){
 			break;
 		}
@@ -59,23 +68,46 @@ function populate(){
 	}
 }
 $("#nextrow").on("click",function(){
-	if(startingIndex>=100)return;
+	if(startingIndex>=totalRows)return;
 	startingIndex++;
 	$("#startRows").val(startingIndex);
-	$(".data").remove();
-	populate();
+	$(".data").first().remove();
+	$("table").append("<tr class='data'><td class='firstcell'></td><td class='secondcell'></td><td class='thirdcell'></td><td class='fourthcell'></td><td class='fifthcell'></td></tr>");
+
+	firstCell = document.querySelectorAll(".firstcell");
+	secondCell = document.querySelectorAll(".secondcell");
+	thirdCell = document.querySelectorAll(".thirdcell");
+	fourthCell = document.querySelectorAll(".fourthcell");
+	fifthCell = document.querySelectorAll(".fifthcell");
+	m.render(firstCell[noOfRows],startingIndex+noOfRows-1);
+	m.render(secondCell[noOfRows],Locn[startingIndex+noOfRows-1]);
+	m.render(thirdCell[noOfRows],OrdId[startingIndex+noOfRows-1]);
+	m.render(fourthCell[noOfRows],KsnId[startingIndex+noOfRows-1]);
+	m.render(fifthCell[noOfRows],SkuCd[startingIndex+noOfRows-1]);
 });
 
 $("#prevrow").on("click",function(){
 	if(startingIndex<=1)return;
 	startingIndex--;
 	$("#startRows").val(startingIndex);
-	$(".data").remove();
-	populate();
+	$(".data").last().remove();
+	var row = "<tr class='data'><td class='firstcell'></td><td class='secondcell'></td><td class='thirdcell'></td><td class='fourthcell'></td><td class='fifthcell'></td></tr>";
+	$(row).insertAfter(".tr1");
+
+	firstCell = document.querySelectorAll(".firstcell");
+	secondCell = document.querySelectorAll(".secondcell");
+	thirdCell = document.querySelectorAll(".thirdcell");
+	fourthCell = document.querySelectorAll(".fourthcell");
+	fifthCell = document.querySelectorAll(".fifthcell");
+	m.render(firstCell[1],startingIndex);
+	m.render(secondCell[1],Locn[startingIndex-1]);
+	m.render(thirdCell[1],OrdId[startingIndex-1]);
+	m.render(fourthCell[1],KsnId[startingIndex-1]);
+	m.render(fifthCell[1],SkuCd[startingIndex-1]);
 });
 
 $("#lastpage").on("click",function(){
-	startingIndex = 91;
+	startingIndex = totalRows-9;
 	$("#startRows").val(startingIndex);
 	$(".data").remove();
 	populate();
@@ -89,7 +121,7 @@ $("#firstpage").on("click",function(){
 });
 
 $("#nextpage").on("click",function(){
-	if(startingIndex<=90)
+	if(startingIndex<=totalRows-10)
 		startingIndex+=10;
 	$("#startRows").val(startingIndex);
 	$(".data").remove();
